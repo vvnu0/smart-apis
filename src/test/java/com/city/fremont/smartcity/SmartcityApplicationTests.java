10089b88f0c338c3749e71a4126b78cf941373bf
Vishnu Nair <nairvishnumail@gmail.com>
2021-05-15 18:16:40 -0700

    intial api changes

---
+++ src/test/java/com/city/fremont/smartcity/SmartcityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.fremont.smartcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
